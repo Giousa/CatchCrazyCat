14eb60f7a58793685ce9bdf15537441bcf00f5ce
Giousa <65489469@qq.com>
2017-03-07 09:10:38 +0800

    first commit

---
+++ app/src/main/java/com/giousa/catchcrazycat/Playground.java
@@ -0,0 +1,59 @@
+package com.giousa.catchcrazycat;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Description:
+ * Author:zhangmengmeng
+ * Date:2017/3/7
+ * Time:上午9:03
+ */
+
+public class Playground extends SurfaceView {
+
+    public Playground(Context context) {
+        this(context,null);
+    }
+
+    public Playground(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public Playground(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+
+    private void redRaw(){
+
+        //在canvas里面可以绘制
+        Canvas canvas = getHolder().lockCanvas();
+        canvas.drawColor(Color.CYAN);
+        getHolder().unlockCanvasAndPost(canvas);//取消canvas的锁定并将绘图内容更新到界面
+
+    }
+
+    SurfaceHolder.Callback mCallback = new SurfaceHolder.Callback(){
+
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+            
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+    };
+}
